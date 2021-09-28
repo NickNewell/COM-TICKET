5c83b01ecd9f6728ba516211ada7d1f1fef00a04
Nicholas Newell <Nicholas.A.Newell@gmail.com>
2021-09-28 12:53:34 -0500

    Stage 5

---
+++ include/client/login.inc.php
@@ -0,0 +1,81 @@
+<?php
+if(!defined('OSTCLIENTINC')) die('Access Denied');
+
+$email=Format::input($_POST['luser']?:$_GET['e']);
+$passwd=Format::input($_POST['lpasswd']?:$_GET['t']);
+
+$content = Page::lookupByType('banner-client');
+
+if ($content) {
+    list($title, $body) = $ost->replaceTemplateVariables(
+        array($content->getLocalName(), $content->getLocalBody()));
+} else {
+    $title = __('Sign In');
+    $body = __();
+}
+
+?>
+<br>
+    <h1><?php echo 'Login to Create and View Tickets'; ?></h1>
+<!-- 
+    7/1/2021 
+    Remove for Cleanliness
+
+    
+    <p><?php echo Format::display($body); ?></p>
+-->
+
+
+<br>
+<form action="login.php" method="post" id="clientLogin">
+    <?php csrf_token(); ?>
+<div style="display:table-row">
+    <div class="login-box">
+    <strong><?php echo Format::htmlchars($errors['login']); ?></strong>
+    <div>
+        <input id="username" placeholder="<?php echo __('Windows Username'); ?>" type="text" name="luser" size="30" value="<?php echo $email; ?>" class="nowarn">
+    </div>
+    <div>
+        <input id="passwd" placeholder="<?php echo __('Password'); ?>" type="password" name="lpasswd" size="30" value="<?php echo $passwd; ?>" class="nowarn"></td>
+    </div>
+    <p>
+        <input class="btn" type="submit" value="<?php echo __('Sign In'); ?>">
+<?php if ($suggest_pwreset) { ?>
+        <a style="padding-top:4px;display:inline-block;" href="pwreset.php"><?php echo __('Forgot My Password'); ?></a>
+<?php } ?>
+    </p>
+    </div>
+    <div style="display:table-cell;padding: 15px;vertical-align:top">
+<?php
+
+$ext_bks = array();
+foreach (UserAuthenticationBackend::allRegistered() as $bk)
+    if ($bk instanceof ExternalAuthentication)
+        $ext_bks[] = $bk;
+
+if (count($ext_bks)) {
+    foreach ($ext_bks as $bk) { ?>
+<div class="external-auth"><?php $bk->renderExternalLink(); ?></div><?php
+    }
+}
+if ($cfg && $cfg->isClientRegistrationEnabled()) {
+    if (count($ext_bks)) echo '<hr style="width:70%"/>'; ?>
+    <div style="margin-bottom: 5px">
+    <br>
+    <br>
+    <br>
+    <?php echo __('Use the same login that you use to access your computer.'); ?>
+    </div>
+<?php } ?>
+    <div>
+    </div>
+    </div>
+</div>
+</form>
+<br>
+<p>
+<?php
+if ($cfg->getClientRegistrationMode() != 'disabled'
+    || !$cfg->isClientLoginRequired()) {
+} ?>
+</p>
